2defd02a70de89efb64c5ea8cdec08b34077cb5a
Md Nayeem <codernayeem898@gmail.com>
2024-07-04 17:37:21 +0600

    added a template cpp file

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    Solution(){
+        ios_base::sync_with_stdio(0); cin.tie(0);
+    }
+
+    int func(vector<int> v){
+        return 0;
+    }
+
+};
+
+int main(){
+    Solution sol;
+
+    vector<int> v = {1, 2, 3, 4, 5};
+
+    cout << sol.func(v);
+}
